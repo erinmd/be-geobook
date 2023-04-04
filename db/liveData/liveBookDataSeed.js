12581f0224fa7bdded185a6372129510f1130709
01Gibbs <danielgilbert49@hotmail.co.uk>
2023-04-04 11:17:00 +0100

    liveServerBookData complete

---
+++ db/liveData/liveBookDataSeed.js
@@ -0,0 +1,245 @@
+export const liveBookDataSeed = [
+  {
+    title: "To Kill A Mockingbird",
+    author: "Harper Lee",
+    genre: "Crime Drama",
+    posted_by: "Cyril.Russel",
+    location: {
+      type: "Point",
+      coordinates: [-1.5682, 53.8027],
+    },
+    location_description:
+      "Amazing book! The clue for this cache is it's higher than you think!",
+  },
+  {
+    title: "Wild Sargasso Sea",
+    author: "Jean Rhys",
+    genre: "Postmodern",
+    posted_by: "Darion_Morissette16",
+    location: {
+      type: "Point",
+      coordinates: [-1.4813, 53.8027],
+    },
+    location_description: "The central sedimentary is not what it seems!",
+  },
+  {
+    title: "To Kill A Mockingbird",
+    author: "Harper Lee",
+    genre: "Crime Drama",
+    posted_by: "Cyril.Russel",
+    location: {
+      type: "Point",
+      coordinates: [-1.5682, 53.8027],
+    },
+    location_description:
+      "Amazing book! The clue for this cache is it's higher than you think!",
+  },
+  {
+    title: "The Handmaid's Tale",
+    author: "Margaret Atwood",
+    genre: "Dystopian",
+    posted_by: "Miss Sharon Smith",
+    location: {
+      type: "Point",
+      coordinates: [-1.6002, 53.8135],
+    },
+    location_description:
+      "Took me ages to find this one! Find the cache by the statue of the feminine figure",
+  },
+  {
+    title: "Litte Women",
+    author: "Louisa May Alcott",
+    genre: "Feminist Fiction",
+    posted_by: "Annetta_Parker",
+    location: {
+      type: "Point",
+      coordinates: [-1.5319, 53.7857],
+    },
+    location_description: "I knew that piece of turf was out of place!",
+  },
+  {
+    title: "A Christmas Carol",
+    author: "Charles Dickens",
+    genre: "Victorian Literature",
+    posted_by: "Imani.West83",
+    location: {
+      type: "Point",
+      coordinates: [-1.4585, 53.7713],
+    },
+    location_description: "Tiny Tim was the true villain!",
+  },
+  {
+    title: "War and Peace",
+    author: "Leo Tolstoy",
+    genre: "Epic Novel",
+    posted_by: "Sienna32",
+    location: {
+      type: "Point",
+      coordinates: [-1.5326, 53.7708],
+    },
+    location_description:
+      "Very challenging! Found near the statue! Great cache!",
+  },
+  {
+    title: "The Wind in the Willows",
+    author: "Kenneth Grahame",
+    genre: "Children's",
+    posted_by: "Sienna32",
+    location: {
+      type: "Point",
+      coordinates: [-1.5051, 53.8179],
+    },
+    location_description: "Look closely at the street signs!",
+  },
+  {
+    title: "The Time Machine",
+    author: "H.G. Wells",
+    genre: "Science Fiction",
+    posted_by: "Sienna32",
+    location: {
+      type: "Point",
+      coordinates: [-1.5776, 53.8295],
+    },
+    location_description: "The clock! The clock! AHHH",
+  },
+  {
+    title: "The Count of Monte Cristo",
+    author: "Alexandre Dumas",
+    genre: "Adventure",
+    posted_by: "Adelle_Barton",
+    location: {
+      type: "Point",
+      coordinates: [-1.4597, 53.8434],
+    },
+    location_description: "Clever! The Count by the Bank!",
+  },
+  {
+    title: "Charlie and the Chocolate Factory",
+    author: "Roald Dahl",
+    genre: "Children's",
+    posted_by: "Giovanna.Green",
+    location: {
+      type: "Point",
+      coordinates: [-1.4619, 53.8068],
+    },
+    location_description:
+      "I did wonder what the relevance was but then I found the cache near the sweetspot!",
+  },
+  {
+    title: "Crime and Punishment",
+    author: "Fyodor Dostoevsky",
+    genre: "Political Fiction",
+    posted_by: "Cassandra_Rohan19",
+    location: {
+      type: "Point",
+      coordinates: [-1.4602, 53.7761],
+    },
+    location_description:
+      "No matter how far you walk, you will always be guilty (Crown Court)!",
+  },
+  {
+    title: "Les Miserables",
+    author: "Victor Hugo",
+    genre: "Tragedy",
+    posted_by: "Cassandra_Rohan19",
+    location: {
+      type: "Point",
+      coordinates: [-1.5898, 53.8123],
+    },
+    location_description: "That cache made me feel les miserables! Good hide!",
+  },
+  {
+    title: "Alice's Adventures in Wonderland",
+    author: "Lewis Carroll",
+    genre: "Children's",
+    posted_by: "Liana_Stehr",
+    location: {
+      type: "Point",
+      coordinates: [-1.4667, 53.8074],
+    },
+    location_description:
+      "How you've made the rabbit stay still for so long is beyond me!",
+  },
+  {
+    title: "Catch-22",
+    author: "Joseph Heller",
+    genre: "Political Satire",
+    posted_by: "Mertie.Rice73",
+    location: {
+      type: "Point",
+      coordinates: [-1.6122, 53.8452],
+    },
+    location_description:
+      "Quote: Let's take a drive into the middle of nowhere with a packet of Marlboro lights and talk about our lives.",
+  },
+  {
+    title: "Great Expectations",
+    author: "Charles Dickens",
+    genre: "Coming of Age",
+    posted_by: "Dolores81",
+    location: {
+      type: "Point",
+      coordinates: [-1.6102, 53.8055],
+    },
+    location_description:
+      "Quote: “Pause you who read this, and think for a moment of the long chain of iron or gold, of thorns or flowers, that would never have bound you, but for the formation of the first link on one memorable day.” ",
+  },
+  {
+    title: "Circe",
+    author: "Madeline Miller",
+    genre: "Drama",
+    posted_by: "Patricia Lesch",
+    location: {
+      type: "Point",
+      coordinates: [-1.6271, 53.7668],
+    },
+    location_description:
+      "It's as high as you think it could be without being out of arm's reach!",
+  },
+  {
+    title: "The Lord of the Rings",
+    author: "J. R. R. Tolkien",
+    genre: "Fantasy",
+    posted_by: "Kara_Raynor4",
+    location: {
+      type: "Point",
+      coordinates: [-1.5205, 53.8294],
+    },
+    location_description:
+      "Hard to find cache! Definitely for experts! Found at the Shire themed Bar",
+  },
+  {
+    title: "All 6 Game of Thrones Books",
+    author: "George R.R. Martin",
+    genre: "Fantasy",
+    posted_by: "Kara_Raynor4",
+    location: {
+      type: "Point",
+      coordinates: [-1.4849, 53.7754],
+    },
+    location_description:
+      "Honestly, still unsure how you squeezed all 6 into here. Good thing he hasn't wrote the seventh!",
+  },
+  {
+    title: "Dracula",
+    author: "Bram Stoker",
+    genre: "Horror",
+    posted_by: "Salma_Schoen",
+    location: {
+      type: "Point",
+      coordinates: [-1.4933, 53.8447],
+    },
+    location_description: "Great read! Cache in the trellis by the bus stop!",
+  },
+  {
+    title: "The Cat in the Hat",
+    author: "Dr. Seuss",
+    genre: "Children's",
+    posted_by: "Salma_Schoen",
+    location: {
+      type: "Point",
+      coordinates: [-1.6137, 53.8435],
+    },
+    location_description: "Leeds Dog's Trust have all the answers!",
+  },
+];
